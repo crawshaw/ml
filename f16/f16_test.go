e86104f2b864da6635f6f118fc7f2749612d4a96
David Crawshaw <crawshaw@google.com>
2014-01-05 15:51:21 -0800

    f16: tests for Sub and Mul

--- f16/f16_test.go
+++ f16/f16_test.go
@@ -33,6 +33,33 @@ func TestAdd(t *testing.T) {
 	}
 }
 
+func TestSub(t *testing.T) {
+	wrk := mkf16([]float32{1, 3, 4, 5, 6, 7, 8, 8})
+	tosub := mkf16([]float32{1, 2, 2, 2, 2, 2, 2, 1})
+	want := []float32{0, 1, 2, 3, 4, 5, 6, 7}
+
+	Sub(wrk, tosub)
+
+	got := fromf16(wrk)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("got %v != want %v", got, want)
+	}
+}
+
+func TestMul(t *testing.T) {
+	wrk := mkf16([]float32{1, 3, 0.5, 4, 5, 7, 8, 8})
+	want := []float32{2, 6, 1, 8, 10, 14, 16, 16}
+
+	Mul(wrk, 2)
+
+	got := fromf16(wrk)
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("got %v != want %v", got, want)
+	}
+}
+
 func BenchmarkAdd(b *testing.B) {
 	b.StopTimer()
 	src1, src2 := mkAddSrcs()
